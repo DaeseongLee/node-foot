60a7ef14cd4653ffe4c0b88a4b306d09c949aa2c
DaeseongLee <hsl5539@gmail.com>
2021-03-25 23:12:54 +0900

    init pages

---
+++ front/pages/register.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Register = (props) => (
+    <h2>Register</h2>
+);
+
+export default Register;
---
+++ front/pages/roomDetail.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RoomDetail = (props) => (
+    <h1>RoomDetail</h1>
+);
+
+export default RoomDetail;
