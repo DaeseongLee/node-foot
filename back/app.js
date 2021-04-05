c45cba3c0b6135bcdec03e9548920d8137b7dbb3
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-05 17:01:12 +0900

    Add error Middleware

--- back/app.js
+++ back/app.js
@@ -21,7 +21,21 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send("hello express");
-})
+});
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 app.listen(app.get('port'), () => {
     console.log('서버 실행 중');
 });
